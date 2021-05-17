2c724ac46d918064e2a0fbdb9818495578185584
Ducktrasher <arielvieyra99@gmail.com>
2021-05-17 07:09:22 -0500

    This calls print function using double quotes which aoutmatically insert the null charactrer at then end fo the array (adds a second string) Also much easier to type

--- Reads files (chili)/Source.cpp
+++ Reads files (chili)/Source.cpp
@@ -7,7 +7,9 @@ void print(const char *s) {
 int main (){
 
 	char msg[] = { 's','a','s','o','r','i',0 };
+	const char msg2[] = { "scorpion" };
 	print(msg);
+	print(msg2);
 	while (!_kbhit());
 	
 	return 0;
