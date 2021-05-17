5b3793131d8c4c216f4c2dedfc91d80784052015
Ducktrasher <arielvieyra99@gmail.com>
2021-05-17 08:37:14 -0500

    This uses ifstream name("nameoffile.txt") to locate file chili's version

--- Reads files (chili)/Source.cpp
+++ Reads files (chili)/Source.cpp
@@ -1,16 +1,15 @@
 #include <conio.h>
-void print(const char *s) {
-	for (; *s != 0; s++) {
-		_putch(*s);
-	}
-}
-int main (){
+#include <fstream>
+#include <iostream>
+using namespace std;
 
-	char msg[] = { 's','a','s','o','r','i',0 };
-	const char msg2[] = { "scorpion" };
-	print(msg);
-	print(msg2);
-	while (!_kbhit());
-	
+int main() {
+	ifstream in("chili.txt");
+
+	for (char c = in.get(); in.good(); c = in.get()) {
+		_putch(c);
+	}
+
+	while (!_kbhit);
 	return 0;
 }
